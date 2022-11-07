3ad137cf043906d5d9013d2d38d65e4d765a74e6
Alexandr Rutkovskij <kitanoyoru@protonmail.com>
2022-11-07 13:44:10 +0300

    Implemented server struct

---
+++ internal/server/server.go
@@ -0,0 +1,31 @@
+package server
+
+import (
+  "context"
+  "net/http"
+
+  "github.com/kitanoyoru/kitaShop-backend/internal/config"
+)
+
+type Server struct {
+  httpServer *http.Server
+}
+
+func NewServer(cfg *config.Config, handler http.Handler) *Server {
+  return &Server {
+    httpServer: &http.Server {
+      Addr: ":" + cfg.HTTP.Port,
+      Handler: handler,
+      ReadTimeout: cfg.HTTP.ReadTimeout,
+      WriteTimeout: cfg.HTTP.WriteTimeout,
+    },
+  }
+}
+
+func (s *Server) Start() error {
+  return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Stop(ctx context.Context) error {
+  return s.httpServer.Shutdown(ctx)
+}
